c4c97ec40a6c7c5f905141cd72e847d86dce2375
TosinJoseph <tosinjosephoffice2@gmail.com>
2025-04-11 15:44:42 -0700

    commit

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -5,17 +5,34 @@ import 'swiper/css'
 const Shop = () => {
   return (
     <div className='px-4 md:px-20 lg:px-30 mx-auto justify-center items-center'>
-        <h2 className='text-center mx-auto font-bold overflow-hidden pt-2 w-full text-4xl'>Man & Woman Fashion</h2>
+        <h2 className='text-center mx-auto font-bold overflow-hidden pt-2 w-full text-5xl'>Man & Woman Fashion</h2>
      
        
-       <Swiper className='min-h-screen' >
+       <Swiper  className='px-4 min-h-screen py-20 md:px-20 lg:px-30'
+       spaceBetween={40}
+       breakpoints={{
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      }}
+        >
         {
         data.map((item, i)=>(
-            <SwiperSlide key={i} className='grid justify-center items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
-             <div className='bg-white  rounded-sm shadow-2xl w-full md:w-1/3 '>
-                <h3>{item.name}</h3>
-                <p>price ${item.price}</p>
-                <img src={item.image} alt="" />
+            <SwiperSlide key={i} className=' justify-center items-center mt-10'>
+             <div className='bg-white mt-12   rounded-sm shadow-2xl h-140  '>
+                <h3 className='text-2xl pt-4 font-bold text-center'>{item.name}</h3>
+                <p className='text-xl pt-4 font-semibold text-center'><span className='text-orange-700'>price</span> ${item.price}</p>
+                <img className='justify-center flex mx-auto items-center mt-6' src={item.image} alt="" />
+                <div className='flex justify-between px-6 pt-3'>
+                  <p className='font-bold text-xl text-orange-700 cursor-pointer'>Buy Now</p>
+                  <p className='cursor-pointer'>See More</p>
+                </div>
              </div>
              
             </SwiperSlide>
